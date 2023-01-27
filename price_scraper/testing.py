172bed0dd1243ca55669cb681d09816b49e7efd6
Archie Tilling <314988@edu.mkcollege.ac.uk>
2023-01-27 15:20:52 +0000

    working with discord message, needs to be hosted from vps

--- price_scraper/testing.py
+++ price_scraper/testing.py
@@ -4,10 +4,58 @@ from selenium.webdriver.chrome.options import Options
 import time
 import requests
 
+webhook_url = "https://discord.com/api/webhooks/1068521584769376349/lf6c7SGCwYxX__-_yqk2qSQIcIaA2SlEhV-i3INvThyq_N1XP5DEWU71y_4wwRAasPRj"
+
 driver = webdriver.Chrome()
-driver.get('https://www.cclonline.com/pc-components/graphics-cards/nvidia-chipset-graphics-cards/')
+driver.get('https://www.cclonline.com/pc-components/graphics-cards/nvidia-chipset-graphics-cards/attributeslist/1069698,1069694,1069691,1069711,1069692/')
 
-header = driver.find_element_by_class("productListContainer pt-3")
+products =  driver.find_elements(By.CLASS_NAME,"productListOverlayWrapper")
 
-for i in range(header):
-    
+def formatElements(elm):
+    return elm.text
+
+discordMessage = []
+for product in products:#this will loop 20 times for the amount of itmes on the page
+
+    title = product.find_element(By.CLASS_NAME, "productList_Middle_Left").find_element(By.TAG_NAME, "a") #this gets the "a" tag where the name for gpu is
+ 
+    name = title.get_attribute("title")
+    url = title.get_attribute("href")
+
+    try:
+        # Get price
+        spans = product.find_element(By.CLASS_NAME, "price-value-container").find_elements(By.TAG_NAME, "span")#this gets the span where the price for gpu is
+        priceValues = map(formatElements, spans)#will run formatElements, every item in spans is passed in. This is to get the text inside of the span
+    except: 
+        continue
+    price = ""
+    for j in priceValues:
+        if j.startswith("Was") or j.startswith("inc"):#formatting what is returned
+            continue
+        price += j
+
+    #formatting what is returned, removing £ 
+    price = price.replace("£", "")
+    discordMessage.append({
+        "name": f"{name}",
+        "value": f"Price: {price}\n [Link]({url})"
+        
+    })
+
+
+print(discordMessage)
+#content is discords the message that is sent to discord, total is the varible of the data
+gpu_data = {
+    # 'content': "<@307952129958477824>\n" + discordMessage
+    "embeds": [
+        {
+            "title": "GPU Prices",
+            # "description": discordMessage
+            "fields": discordMessage
+        }
+    ]
+    }
+#sending data
+x = requests.post(webhook_url, json = gpu_data)
+
+print("DONE")
