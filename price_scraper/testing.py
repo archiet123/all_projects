6e2a893a529913ce54bf5170f19dc4d1da2a39ba
Archie Tilling <314988@edu.mkcollege.ac.uk>
2023-01-27 15:36:36 +0000

    working with headerless chrome

--- price_scraper/testing.py
+++ price_scraper/testing.py
@@ -3,12 +3,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
 import requests
+import undetected_chromedriver as uc
 
 webhook_url = "https://discord.com/api/webhooks/1068521584769376349/lf6c7SGCwYxX__-_yqk2qSQIcIaA2SlEhV-i3INvThyq_N1XP5DEWU71y_4wwRAasPRj"
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
+# driver.get('https://www.cclonline.com/pc-components/graphics-cards/nvidia-chipset-graphics-cards/attributeslist/1069698,1069694,1069691,1069711,1069692/')
+
+#this is for headless chrome
+# options = Options()
+options = uc.ChromeOptions() 
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+driver = uc.Chrome(use_subprocess=True, options=options)
 driver.get('https://www.cclonline.com/pc-components/graphics-cards/nvidia-chipset-graphics-cards/attributeslist/1069698,1069694,1069691,1069711,1069692/')
 
+time.sleep(10)
+driver.save_screenshot("lol.png")
 products =  driver.find_elements(By.CLASS_NAME,"productListOverlayWrapper")
 
 def formatElements(elm):
@@ -21,7 +33,7 @@ for product in products:#this will loop 20 times for the amount of itmes on the 
  
     name = title.get_attribute("title")
     url = title.get_attribute("href")
-
+    
     try:
         # Get price
         spans = product.find_element(By.CLASS_NAME, "price-value-container").find_elements(By.TAG_NAME, "span")#this gets the span where the price for gpu is
@@ -43,19 +55,19 @@ for product in products:#this will loop 20 times for the amount of itmes on the 
     })
 
 
-print(discordMessage)
-#content is discords the message that is sent to discord, total is the varible of the data
-gpu_data = {
-    # 'content': "<@307952129958477824>\n" + discordMessage
-    "embeds": [
-        {
-            "title": "GPU Prices",
-            # "description": discordMessage
-            "fields": discordMessage
+if len(discordMessage) >0:    
+    #content is discords the message that is sent to discord, total is the varible of the data
+    gpu_data = {
+        # 'content': "<@307952129958477824>\n" + discordMessage
+        "embeds": [
+            {
+                "title": "GPU Prices",
+                # "description": discordMessage
+                "fields": discordMessage
+            }
+        ]
         }
-    ]
-    }
-#sending data
-x = requests.post(webhook_url, json = gpu_data)
+    #sending data
+    x = requests.post(webhook_url, json = gpu_data)
 
 print("DONE")
