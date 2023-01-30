21c50d920005b38bfe92236bbd87aec4ec8bcba9
Archie Tilling <314988@edu.mkcollege.ac.uk>
2023-01-30 13:55:21 +0000

    setup for cron cript

--- discord_ping/main.py
+++ discord_ping/main.py
@@ -12,20 +12,20 @@ current_time = now.strftime("%H:%M")
 print("Current Time =", current_time)
 
 
-counter = 0
-while current_time != "16:20" and current_time != "04:20":
-    now = datetime.now()
-    current_time = now.strftime("%H:%M")
-    counter = 0
-else:
+# counter = 0
+# while current_time != "16:20" and current_time != "04:20":
+#     now = datetime.now()
+#     current_time = now.strftime("%H:%M")
+#     counter = 0
+# else:
     
-    if counter == 0:
-        counter = 1
-        message = "its 420"
-        post_message = {
-         'content': "<@523197640347746325>\n" + message
-        }
-        x = requests.post(webhook_url, json = post_message)
+if counter == 0:
+    counter = 1
+    message = "its 420! 😩"
+    post_message = {
+        'content': "<@523197640347746325>\n" + message
+    }
+    x = requests.post(webhook_url, json = post_message)
 
-    else:
-        print("already sent")
+else:
+    print("already sent")
