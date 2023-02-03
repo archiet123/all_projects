24922ce2028e094c6af353d52ac23f6036f8d90f
Archie Tilling <314988@edu.mkcollege.ac.uk>
2023-02-03 13:04:03 +0000

    fixes

--- monkeytype_hacks/main.py
+++ monkeytype_hacks/main.py
@@ -15,18 +15,16 @@ letter_list = []
 
 time.sleep(10)
 #accept = driver.find_element(By.CLASS_NAME, 'acceptAll').click()
-for i in range(5):
-    get_words =  driver.find_element(By.ID,"words") #the parent div that needs to be looped over
-    words = (get_words.text.split())
-    print(words)
-    space = " "
 
-   
-    
-    for i in words:
-        actions.send_keys(i)
-        actions.send_keys(space)
-        actions.perform()
-    
-    time.sleep(300)
-    driver.close()
+get_words =  driver.find_element(By.ID,"words") #the parent div that needs to be looped over
+words = (get_words.text.split())
+print(words)
+space = " "   
+
+for i in words:
+    actions.send_keys(i)
+    actions.send_keys(space)
+    actions.perform()
+
+time.sleep(300)
+driver.close()
