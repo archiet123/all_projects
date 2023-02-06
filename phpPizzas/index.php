1699c1a23e9e4f644b555c6214edcc1381dde1c6
Archie Tilling <314988@edu.mkcollege.ac.uk>
2023-02-06 15:58:03 +0000

    php pizzas init commit

---
+++ phpPizzas/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        <div class="logo">
+            <img src="/assets/logo.png" alt="pizza logo" href="index.php" width="100px"></img>
+        </div>
+        <div class="sign-in">
+            <a class="button" href="signIn.php">Register</a>
+
+                <div class="spacer"></div>
+
+            <a class="button" href="signIn.php">Sign in</a>
+        </div>
+    </div>
+    <section class="main">
+        <div class="mainContainer">
+
+            <img src="/assets/italy-flag.png" alt="flag" class="banner container" height="50%"></img>
+            <h1 class="title">Pizza Menu</h1>
+        </div>
+    </section>
+</body>
+</html>
