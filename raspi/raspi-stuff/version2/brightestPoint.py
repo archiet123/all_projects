09f09158363a379b4ca4f0b926f3345573826151
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-03-01 23:33:54 +0000

    PROGRAM WORKS

--- raspi/raspi-stuff/version2/brightestPoint.py
+++ raspi/raspi-stuff/version2/brightestPoint.py
@@ -9,29 +9,49 @@ import cv2
 #camera = PiCamera()
 #camera.rotation = -180
 
+def getSelector():
+	if final > 48 and final < 60:
+		selector = 1
+		
+
+toRemove = [')']
+characters = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+final_string = ""
 
 img = cv2.imread(f'first_image.jpg')#reading init pic
 cropped = img[260:650,460:1460]#setting bounds of whole punchcard
-
 whole = plt.imsave("wholecard.jpg", cropped)
 
-
-column0 = img[260:650, 470:480]#column0 bounds  
-zero = plt.imsave("newimage0.jpg", column0)
-
+column0 = img[260:650, 470:480]#column0 bounds 
+#new = plt.imsave("new1.jpg",column1)
 column1 = img[260:650, 483:493]#column1 bounds
-one = plt.imsave("newimage1.jpg", column1)
-
+#new = plt.imsave("new1.jpg",column1)
 column2 = img[260:650, 496:506]#column1 bounds
-one = plt.imsave("newimage2.jpg", column2)
+#new = plt.imsave("new2.jpg",column2)
+
+#image = (f'column{index}') 	
 
 
-shape =column1.shape
-gray = cv2.cvtColor(column1, cv2.COLOR_BGR2GRAY)
-minMaxLoc = cv2.minMaxLoc(gray)
-yRegion = str(minMaxLoc)[27:30]#the second value in squre brackets '30' will sometimes return a bracket, find a method to strip this character.
 
-print(f'the size of the image is: {shape}')
-print(f'the brightest part of the image, darkest part of the image, x coord, y coord{minMaxLoc}')
-print(f'y region: {yRegion}')
+for index in range(0,3):
+	read = cv2.imread(f"column{index}.jpg")
+	show = cv2.imshow("show", read)
+	
 
+	gray = cv2.cvtColor(read, cv2.COLOR_BGR2GRAY)
+#	blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+#	thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)[1]
+#	erode = cv2.erode(thresh, None, iterations=2)
+#	saveErode = plt.imsave(f'eroded{index}.jpg')
+#
+	minMaxLoc = cv2.minMaxLoc(read)
+#
+	yRegion = str(minMaxLoc)[27:30]
+#	final = yRegion.replace(')', '')
+#	selector = getSelector()
+#	
+#	
+	print(f'the size of the image is: {shape}')
+#	print(f'the brightest part of the image, darkest part of the image, x coord, y coord{minMaxLoc}')
+	print(f'y region: {yRegion}')
+cv2.waitKey(0)
--- raspi/raspi-stuff/version2/testing.py
+++ raspi/raspi-stuff/version2/testing.py
@@ -1,9 +1,45 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-im=cv2.imread('column0.ppm')
-g = cv2.imread(im, cv2.COLOR_BGR2GRAY)
-a = np.array(g)
-print (a.max(), np.unravel_index(a.argmax(), a.shape))
-a[188:208,478:498] =255
-cv2.imshow('aaa',a)
+#img = cv2.imread(f'first_image.jpg')#reading init pic
+
+characters = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+finalString = ""
+
+def getSelector():
+	if final > 48 and final < 60:
+		selector = 1
+		return(selector)
+	elif final > 83 and final < 90:
+		selector = 2
+		return(selector)
+	elif final > 119 and final <130:
+		selector = 3
+		return(selector)
+
+for index in range(0,3):
+	read = cv2.imread(f"column{index}.jpg")
+	#show = cv2.imshow("show", read)
+
+	gray = cv2.cvtColor(read, cv2.COLOR_BGR2GRAY)
+	blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+	thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)[1]
+	erode = cv2.erode(thresh, None, iterations=2)
+
+	#new = plt.imsave("new.jpg",erode)
+	#cv2.imwrite("new.jpg", erode)
+
+	minMaxLoc = cv2.minMaxLoc(gray)
+
+	yRegion = str(minMaxLoc)[26:29]
+	Replace = yRegion.replace(' ', '')
+	final = int(Replace)		
+	selector = getSelector()
+	
+	finalString += characters[index]	
+
+	cv2.waitKey(0)
+
+#print("\n")
+print(f"The final string is: {finalString}")
