06354c534087a6e2848a9b29bdfdcd697378392d
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-03-07 12:24:38 +0000

    1, 2, 3 are working

--- raspi/raspi-stuff/version2/brightestPoint.py
+++ raspi/raspi-stuff/version2/brightestPoint.py
@@ -17,10 +17,10 @@ def getCharacter(final):
 	if final > 48 and final < 60:
 		selector = 1
 		return selector
-	elif final > 84 and final < 90:
+	elif final > 79 and final < 90:
 		selector = 2
 		return selector
-	elif final > 119 and final < 130:
+	elif final > 113 and final < 130:
 		selector = 3
 		return selector
 
@@ -28,19 +28,19 @@ img = cv2.imread(f'first_image.jpg')#reading init pic
 cropped = img[260:650,460:1460]#setting bounds of whole punchcard
 
 column0 = img[260:650, 470:480]#column0 bounds  
-
+plt.imsave("testing0.jpg", column0)
 column1 = img[260:650, 483:493]#column1 bounds
-
+plt.imsave("testing1.jpg", column1)
 column2 = img[260:650, 496:506]#column1 bounds
-
+plt.imsave("testing2.jpg", column2)
 
 toRemove = ["/", ")", "("]
 
 
 for index in range(0,3):
-	imageName = (f'column{index}') # save images as newimage{column index}    	
+	imageName = (f'testing{index}') # save images as newimage{column index}    	
 	read = cv2.imread(f'{imageName}.jpg')#this will need to loop through all images that need to be read	
-	shape =read.shape	
+	#shape =read.shape	
 	gray = cv2.cvtColor(read, cv2.COLOR_BGR2GRAY) #the first parameter is the image that is read by cv2
 	
 	blurred = cv2.GaussianBlur(gray, (11, 11), 0)	
@@ -54,7 +54,7 @@ for index in range(0,3):
 	yRegion = str(minMaxLoc)[25:28]#just gets the Y axis 	
 	final = yRegion.replace(')', '')#replacing the bracket that is returned with tripple digit coords
 	
-	print(f'the size of the image is: {shape}')	
+	#print(f'the size of the image is: {shape}')	
 	print(f'the brightest part of the image, darkest part of the image, x coord, y coord{minMaxLoc}')	
 	print(f'y region: {final}')
 	
