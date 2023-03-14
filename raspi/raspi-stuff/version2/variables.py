9983551c7e0b35a688916bdf6ee4047a1b0fce67
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-03-14 15:25:36 +0000

    added seperate file for variables

---
+++ raspi/raspi-stuff/version2/variables.py
@@ -0,0 +1,54 @@
+import re
+import numpy as np
+import cv2
+#from picamera import PiCamera
+from PIL import Image, ImageChops
+import PIL
+import matplotlib.pyplot as plt
+import cv2
+
+img = cv2.imread(f'assets/card.jpg')#reading init pic
+
+column0 = img[260:650, 470:480]#column0 bounds  
+plt.imsave("assets/testing0.jpg", column0)
+
+column1 = img[260:650, 483:493]#column1 bounds
+plt.imsave("assets/testing1.jpg", column1)
+
+column2 = img[260:650, 496:506]#column2 bounds
+plt.imsave("assets/testing2.jpg", column2)
+
+column3 = img[260:650, 509:519]
+plt.imsave("assets/testing3.jpg", column3)
+
+column4 = img[260:650, 522:532]
+plt.imsave("assets/testing4.jpg", column4)
+
+column5 = img[260:650, 533:543]
+plt.imsave("assets/testing5.jpg", column5)
+
+column6 = img[260:650, 545:555]
+plt.imsave("assets/testing6.jpg", column6)
+
+column7 = img[260:650, 558: 568]
+plt.imsave("assets/testing7.jpg", column7)
+
+column8 = img[260:650, 571: 581]
+plt.imsave("assets/testing8.jpg", column8)
+
+column9 = img[260:650, 584: 594]
+plt.imsave("assets/testing9.jpg", column9)
+
+column10 = img[260:650, 596:606]
+plt.imsave("assets/testing10.jpg", column10)
+
+'''
+listSelector0 = img[195:230, 470:480]
+plt.imsave("assets/first.jpg", listSelector0)
+
+listSelector1 = img[195:230, 470:480]
+plt.imsave("assets/second.jpg", listSelector1)
+
+listSelector2 = img[195:230, 470:480]
+plt.imsave("assets/third.jpg", listSelector2)
+'''
